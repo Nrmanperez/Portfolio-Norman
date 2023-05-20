b13ddbb14d5d07a4e160015e2363b72f72fb9d00
Nrmanperez <perez.norman30@gmail.com>
2023-05-20 10:02:59 -0600

    structure proyect

---
+++ src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { LandingLayout } from '../../Layout/LandingLayout/LandingLayout'
+import { Home } from '../Home/Home'
+
+export const LandingPage = () => {
+  return (
+    <LandingLayout>
+        <Home/>
+    </LandingLayout>
+  )
+}
