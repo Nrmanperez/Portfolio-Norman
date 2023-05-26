2802bdb0d1085dc113f6335133a81f5aea05a608
Nrmanperez <nperez@nicarao.agency>
2023-05-25 23:24:44 -0600

    changes in the home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,20 +1,34 @@
 import profile from '../../assets/norman.png'
 
 export const Home = () => {
+
   return (
-    <section className="mt-16 grid grid-cols-2 bg-black"
-      // className='mt-16 relative bg-[url("/home/norman/Documents/portafolio/Portfolio-Norman/src/assets/norman.jpg")] bg-cover bg-fixed bg-right w-full h-screen bottom-2'
+    <section 
+      className="mt-16 xl:grid xl:grid-cols-2 sm:flex sm:flex-col sm:justify-center sm:items-center bg-black"
+      style={{ height: `calc(100vh - 64px)` }}
     >
-      <div className='text-center m-auto'>
-        <div className='text-start'>
-          <p className='text-white text-sm'>Hello</p>
-          <p className='text-white text-3xl'>I am <span>Norman</span>Perez</p>
+      <div className='xl:text-center xl:m-auto xl:w-auto sm:mt-16'>
+        <div className='text-start mb-5'>
+          <p className='text-white text-2xl'>Hello</p>
+          <p className='text-white text-4xl'>I am <span className='text-red-500'>Norman </span>Perez</p>
         </div>
         <div>
-          <h1 className='text-white'>
-            <span>I am</span>
-            <span>
-              <b>Web Developer</b>
+          <h1 className="text-white text-6xl">
+            <span className="text-left">
+              I am{' '}
+              <span className="relative">
+                <b>
+                  {Array.from('Web Developer').map((letter, index) => (
+                    <span
+                      key={index}
+                      className={`inline-block animate-fade-in-left animate-delay-${index} text-red-500`}
+                      style={{ animationDuration: '3s', animationIterationCount: 'infinite' }}
+                    >
+                      {letter}
+                    </span>
+                  ))}
+                </b>
+              </span>
             </span>
           </h1>
         </div>
@@ -33,8 +47,8 @@ export const Home = () => {
           </a>
         </div>
       </div>
-      <div className='relative bottom-14'>
-        <img src={profile} alt="Profile Norman" className='max-h-screen w-full' />
+      <div className='relative bottom-14 mt-20'>
+        <img src={profile} alt="Profile Norman" className='xl:w-3/4 sm:w-full sm:h-1/3' style={{ height: '78vh' }} />
       </div>
     </section>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,32 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      animationDelay: {
+        '0': '0s',
+        '1': '1s',
+        '2': '2s',
+        '3': '3s',
+        '4': '4s',
+        // Agrega más valores de retraso según sea necesario
+      },
+      animation: {
+        'fade-out-right': 'fade-out-right 2s ease-in-out',
+        'fade-in-left': 'fade-in-left 2s ease-in-out',
+      },
+      keyframes: {
+        'fade-out-right': {
+          '0%': { opacity: '1', transform: 'translateX(0)' },
+          '100%': { opacity: '0', transform: 'translateX(-100%)' },
+        },
+        'fade-in-left': {
+          '0%': { opacity: '0', transform: 'translateX(100%)' },
+          '100%': { opacity: '1', transform: 'translateX(0)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
