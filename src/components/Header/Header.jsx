1cc95586a773896ec6b20bdc6d179f74eee4289e
Norman Perez <perez.norman30@gmail.com>
2024-11-30 12:15:09 -0600

    fix email header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,27 @@
-import { Disclosure, Menu } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll'
+import { Disclosure, Menu } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
 export const Header = () => {
-
   const user = {
-    name: 'Norman',
-    email: 'nperez@nicarao.agency',
+    name: "Norman",
+    email: "perez.norman30@gmail.com",
     imageUrl:
-      'https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2023/04/portafolio-programador.webp',
-  }
+      "https://www.hostinger.es/tutoriales/wp-content/uploads/sites/7/2023/04/portafolio-programador.webp",
+  };
   const navigation = [
-    { id: 'home', name: 'Home', href: '#', current: false },
-    { id: 'aboutme', name: 'About Me', href: '#', current: false },
-    { id: 'skills', name: 'Skills', href: '#', current: false },
-    { id: 'projects', name: 'Projects', href: '#', current: false },
-  ]
-  
+    { id: "home", name: "Home", href: "#", current: false },
+    { id: "aboutme", name: "About Me", href: "#", current: false },
+    { id: "skills", name: "Skills", href: "#", current: false },
+    { id: "projects", name: "Projects", href: "#", current: false },
+  ];
+
   function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
+    return classes.filter(Boolean).join(" ");
   }
 
   const scrollTo = () => {
-    scroll.scrollTo('skills', {
+    scroll.scrollTo("skills", {
       duration: 500,
       smooth: true,
       offset: -80,
@@ -31,20 +30,25 @@ export const Header = () => {
 
   return (
     <>
-      <header className='fixed top-0 left-0 right-0 z-50'>
+      <header className="fixed top-0 left-0 right-0 z-50">
         <Disclosure as="nav" className="bg-white shadow-sm shadow-black">
           {({ open }) => (
             <>
               <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
-                  <div className="ml-4 flex items-center md:ml-6">
-                    {/* Profile dropdown */}
-                    <Menu as="div" className="relative ml-3">
-                      <div>
-                        <a href='#' className='text-black font-black uppercase tracking-tighter text-4xl leading-10 transition ease-in-out hover:text-red-600 duration-300'>{user.name}</a>
-                      </div>
-                    </Menu>
+                    <div className="ml-4 flex items-center md:ml-6">
+                      {/* Profile dropdown */}
+                      <Menu as="div" className="relative ml-3">
+                        <div>
+                          <a
+                            href="#"
+                            className="text-black font-black uppercase tracking-tighter text-4xl leading-10 transition ease-in-out hover:text-red-600 duration-300"
+                          >
+                            {user.name}
+                          </a>
+                        </div>
+                      </Menu>
                     </div>
                   </div>
                   <div className="hidden md:block">
@@ -62,11 +66,11 @@ export const Header = () => {
                             onClick={scrollTo}
                             className={classNames(
                               item.current
-                                ? 'bg-gray-900 text-white'
-                                : 'text-black hover:text-red-600',
-                              'rounded-md px-3 py-2 text-sm font-medium'
+                                ? "bg-gray-900 text-white"
+                                : "text-black hover:text-red-600",
+                              "rounded-md px-3 py-2 text-sm font-medium"
                             )}
-                            aria-current={item.current ? 'page' : undefined}
+                            aria-current={item.current ? "page" : undefined}
                           >
                             {item.name}
                           </ScrollLink>
@@ -79,9 +83,15 @@ export const Header = () => {
                     <Disclosure.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-black">
                       <span className="sr-only">Open main menu</span>
                       {open ? (
-                        <XMarkIcon className="block h-9 w-9" aria-hidden="true" />
+                        <XMarkIcon
+                          className="block h-9 w-9"
+                          aria-hidden="true"
+                        />
                       ) : (
-                        <Bars3Icon className="block h-9 w-9" aria-hidden="true" />
+                        <Bars3Icon
+                          className="block h-9 w-9"
+                          aria-hidden="true"
+                        />
                       )}
                     </Disclosure.Button>
                   </div>
@@ -91,7 +101,7 @@ export const Header = () => {
               <Disclosure.Panel className="md:hidden">
                 <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
                   {navigation.map((item) => (
-                    <ScrollLink 
+                    <ScrollLink
                       key={item.name}
                       to={item.id}
                       smooth={true}
@@ -103,10 +113,12 @@ export const Header = () => {
                     >
                       <Disclosure.Button
                         className={classNames(
-                          item.current ? 'text-white' : 'text-black hover:text-red-600',
-                          'block rounded-md px-3 py-2 text-base font-medium'
+                          item.current
+                            ? "text-white"
+                            : "text-black hover:text-red-600",
+                          "block rounded-md px-3 py-2 text-base font-medium"
                         )}
-                        aria-current={item.current ? 'page' : undefined}
+                        aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
                       </Disclosure.Button>
@@ -116,11 +128,19 @@ export const Header = () => {
                 <div className="border-t border-gray-700 pb-3 pt-4">
                   <div className="flex items-center px-5">
                     <div className="flex-shrink-0">
-                      <img className="h-10 w-10 rounded-full" src={user.imageUrl} alt="" />
+                      <img
+                        className="h-10 w-10 rounded-full"
+                        src={user.imageUrl}
+                        alt=""
+                      />
                     </div>
                     <div className="ml-3">
-                      <div className="text-base font-medium leading-none text-gray-800">{user.name}</div>
-                      <div className="text-sm font-medium leading-none text-gray-800">{user.email}</div>
+                      <div className="text-base font-medium leading-none text-gray-800">
+                        {user.name}
+                      </div>
+                      <div className="text-sm font-medium leading-none text-gray-800">
+                        {user.email}
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -130,5 +150,5 @@ export const Header = () => {
         </Disclosure>
       </header>
     </>
-  )
-}
+  );
+};
